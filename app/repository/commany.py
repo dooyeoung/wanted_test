8149245ed8baca1bad4fb42a268e94979bc022d6
dooyeoung <dooyeoung@gmail.com>
2022-09-03 20:16:48 +0900

    데이터 타입 수정

--- app/repository/commany.py
+++ app/repository/commany.py
@@ -3,19 +3,19 @@ from typing import Optional, Sequence
 
 from sqlalchemy.orm.session import Session
 
-from app.model.company import Company, CompanyName
+from app.model.company import CompanyName
 
 
 class CompanyRepository(abc.ABC):
     @abc.abstractmethod
     def get_commany_name_by_name(
         self, session: Session, **parameters
-    ) -> Optional[Company]:
+    ) -> Optional[CompanyName]:
         pass
 
     def search_commany_name_by_query(
         self, session: Session, **parameters
-    ) -> Sequence[Company]:
+    ) -> Sequence[CompanyName]:
         pass
 
 
@@ -24,7 +24,7 @@ class SQLAlchemyCompanyRepository(CompanyRepository):
         self,
         session: Session,
         name: str,
-    ) -> Optional[Company]:
+    ) -> Optional[CompanyName]:
         return session.query(CompanyName).filter(CompanyName.name == name).one_or_none()
 
     def search_commany_names_by_query(
@@ -32,7 +32,7 @@ class SQLAlchemyCompanyRepository(CompanyRepository):
         session: Session,
         query: str,
         language: str,
-    ) -> Sequence[Company]:
+    ) -> Sequence[CompanyName]:
         return (
             session.query(CompanyName)
             .filter(
